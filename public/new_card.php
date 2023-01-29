89e292a2405f5d640e037d5ccbd5199fb4983dc9
DevShivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2023-01-29 21:23:09 +0100

    Intégration API TinyMCE

--- public/new_card.php
+++ public/new_card.php
@@ -12,7 +12,8 @@ include '../src/valid_login.php';
     <title>Créez votre carte à envoyer sur SendCards</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../style/style.css"><script src="https://cdn.tiny.cloud/1/qj7j0nufc3ewmuk77itllcnlnkot53bxad5kq4u5a5knykd6/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="https://cdn.tiny.cloud/1/qj7j0nufc3ewmuk77itllcnlnkot53bxad5kq4u5a5knykd6/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 <body class="container-fluid">
 
@@ -93,7 +94,13 @@ include '../src/valid_login.php';
       <?php } ?>
   </div>
   <!-- *** fin corps du site *** -->
-
+  <script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+    });
+  </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>

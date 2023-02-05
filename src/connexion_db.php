5006875d13832b9ddd7fbb2bee7ee3c26b76e2af
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2023-02-05 21:28:02 +0100

    Delete connexion_db.php

--- src/connexion_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try { 
-    // Connexion BDD en développement
-    $pdo = new PDO("mysql:host=localhost;dbname=sendcards","root","");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
-}
